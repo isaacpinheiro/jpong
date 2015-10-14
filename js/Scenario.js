647c3646990a9c8a2e92d9f75a2c7f588ce475f7
Isaac Pinheiro <isaacpnhr@gmail.com>
2015-10-14 03:00:44 -0300

    Finalizado.

--- js/Scenario.js
+++ js/Scenario.js
@@ -7,6 +7,10 @@ var Scenario = function(){
   var _player1;
   var _player2;
 
+  var rand = function(min, max){
+    return parseInt(Math.random() * (max - min) + min);
+  };
+
   this.setId = function(id){
     _id = id;
   };
@@ -39,8 +43,67 @@ var Scenario = function(){
     return _player2;
   };
 
-  this.run = function(){
-    // TODO
+  this.run = function(b){
+
+    var vertical;
+    var horizontal;
+
+    if(rand(0,2) == 1){
+      vertical = true;
+    }else{
+      vertical = false;
+    }
+
+    if(rand(0,2) == 1){
+      horizontal = true;
+    }else{
+      horizontal = false;
+    }
+
+    var ballMovement = setInterval(function(){
+
+      if(_ball.getY() > 0 && _ball.getY() < 284){
+
+        if(vertical){
+
+          _ball.setY(parseInt(_ball.getY() + 2));
+          b.style.marginTop = _ball.getY()+'px';
+
+          if(_ball.getY() >= 224 && ((_ball.getX() + 15) >= _player1.getX() && _ball.getX() <= (_player1.getX() + 69))){
+            vertical = !vertical;
+          }
+
+        }else{
+
+          _ball.setY(parseInt(_ball.getY() - 2));
+          b.style.marginTop = _ball.getY()+'px';
+
+          if (_ball.getY() <= 62 && ((_ball.getX() + 15) >= _player2.getX() && _ball.getX() <= (_player2.getX() + 69))){
+            vertical = !vertical;
+          }
+
+        }
+
+        if(horizontal){
+          _ball.setX(parseInt(_ball.getX() + 2));
+          b.style.marginLeft = _ball.getX()+'px';
+          if (_ball.getX() == 284) horizontal = !horizontal;
+        }else{
+          _ball.setX(parseInt(_ball.getX() - 2));
+          b.style.marginLeft = _ball.getX()+'px';
+          if (_ball.getX() == 0) horizontal = !horizontal;
+        }
+
+      }else if(_ball.getY() == 284){
+        window.alert('Player 2 win!');
+        clearInterval(ballMovement);
+      }else if(_ball.getY() == 0){
+        window.alert('Player 1 win!');
+        clearInterval(ballMovement);
+      }
+
+    }, 70);
+
   };
 
 }
